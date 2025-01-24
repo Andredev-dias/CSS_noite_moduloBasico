88e5b3087855b95c760436c2239e4838166ead6a
Andre Dias <andredias.curitiba@gmail.com>
2025-01-24 19:17:45 -0300

    sem pto e virgula

--- CSS_basico/Aula03/script.js
+++ CSS_basico/Aula03/script.js
@@ -1,31 +1,31 @@
 function abrir() {
-  var modal = document.getElementById("modal");
-  modal.style.display = "flex";
+  var modal = document.getElementById("modal")
+  modal.style.display = "flex"
 }
 
 function fecharModal() {
-  var modal = document.getElementById("modal");
-  modal.style.display = "none";
+  var modal = document.getElementById("modal")
+  modal.style.display = "none"
 }
 
 function fecharAlert() {
-  var alert = document.getElementById("alert");
-  alert.style.display = "none";
+  var alert = document.getElementById("alert")
+  alert.style.display = "none"
 }
 
 function enviar(event) {
   event.preventDefault();
-  const nome = document.getElementById("nome").value;
-  const assunto = document.getElementById("msg").value;
-  const alert = document.getElementById("alert");
-  alert.style.display = "none";
-  document.getElementById("nome").style.background = "#ffffff";
-  document.getElementById("msg").style.background = "#ffffff";
+  const nome = document.getElementById("nome").value
+  const assunto = document.getElementById("msg").value
+  const alert = document.getElementById("alert")
+  alert.style.display = "none"
+  document.getElementById("nome").style.background = "#ffffff"
+  document.getElementById("msg").style.background = "#ffffff"
 
   if (!nome || !assunto) {
-    alert.style.display = "flex";
-    document.getElementById("nome").style.background = "lightpink";
-    document.getElementById("msg").style.background = "lightpink";
+    alert.style.display = "flex"
+    document.getElementById("nome").style.background = "lightpink"
+    document.getElementById("msg").style.background = "lightpink"
     return;
   }
 
